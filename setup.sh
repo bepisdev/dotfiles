4280a06e967d9878734ad0374775b4e46b55ed80
Josh Burns <joshyburnss@gmail.com>
2025-05-13 17:09:00 +1000

    install dotfile packages and setup doom emacs

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Download package manifest
+curl https://raw.githubusercontent.com/bepisdev/dotfiles/refs/heads/main/pkg.csv > /tmp/pkg.csv
+
 # Process package manifest
-CSV_FILE="pkg.csv"
+CSV_FILE="/tmp/pkg.csv"
 tail -n +2 "$CSV_FILE" | while IFS=',' read -r pkgname pkgtype clioptions
 do
     echo "Installing Package: $pkgname"
@@ -25,4 +28,12 @@ done
 
 # Link dotfiles with stow
 DOTFILES_DIR="$HOME/Code/dotfiles"
+mkdir -p $DOTFILES_DIR
+
 (cd $DOTFILES_DIR && stow zsh -t $HOME)
+(cd $DOTFILES_DIR && stow emacs -t $HOME)
+(cd $DOTFILES_DIR && stow git -t $HOME)
+
+# Run doom emacs setup utility
+~/.emacs.d/bin/doom install
+~/.emacs.d/bin/doom sync
