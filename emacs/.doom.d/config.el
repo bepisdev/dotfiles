e8700863e292a61ca0ee6ba835bde2af57bb1bfa
Josh Burns <joshyburnss@gmail.com>
2025-05-13 16:03:01 +1000

    update banner and personal info in config

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -6,10 +6,10 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
+(setq user-full-name "Josh Burns"
+      user-mail-address "joshyburnss@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
+Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
 ;; - `doom-font' -- the primary font to use
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
