c02c500ca6348ed1a0a225af9e220a75561bb64d
Josh Burns <joshyburnss@gmail.com>
2025-05-13 18:24:50 +1000

    spaceship prompt config

--- zsh/.spaceshiprc.zsh
+++ zsh/.spaceshiprc.zsh
@@ -1,6 +1,20 @@
 # Display username always
-SPACESHIP_USER_SHOW=always
 SPACESHIP_PROMPT_SEPARATE_LINE=false
+SPACESHIP_SUDO_SHOW=true
 
-# Add prompt sections
+# User@Host in prompt
+SPACESHIP_USER_SHOW=always
+SPACESHIP_USER_SUFFIX=""
+SPACESHIP_HOST_SHOW=always
+SPACESHIP_HOST_SHOW_FULL=true
+SPACESHIP_HOST_PREFIX="@"
+
+# Directory
+spaceship remove dir
+spaceship add dir --after host
+SPACESHIP_DIR_PREFIX="["
+SPACESHIP_DIR_SUFFIX="] "
+
+# Display count of background jobs
 spaceship add jobs
+SPACESHIP_JOBS_AMOUNT_THRESHOLD=0
