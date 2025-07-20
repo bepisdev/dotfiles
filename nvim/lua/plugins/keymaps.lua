69aee8d823271de469fa4528036a4a0042b2de52
Josh Burns <joshyburnss@gmail.com>
2025-07-21 00:45:17 +1000

    more keymap options

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -15,30 +15,31 @@ return {
 
 			-- Telescope
 			wk.add({
-				{ "<leader>f", group = "+find"},
-				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files"},
-				{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "live grep"},
-				{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "buffers"},
-				{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "help tags"},
-				{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "recent files"},
+				{ "<leader>f",  group = "+find" },
+				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
+				{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "live grep" },
+				{ "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "buffers" },
+				{ "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "help tags" },
+				{ "<leader>fr", "<cmd>Telescope oldfiles<cr>",   desc = "recent files" },
 			}),
 
 			wk.add({
-				{ "<leader>p", group = "+projects"},
-				{ "<leader>pp", "<cmd>Telescope projects<cr>", desc = "switch project"},
-				{ "<leader>pa", "<cmd>AddProject<cr>", desc = "add project"},
+				{ "<leader>p",  group = "+projects" },
+				{ "<leader>pp", "<cmd>Telescope projects<cr>", desc = "switch project" },
+				{ "<leader>pa", "<cmd>AddProject<cr>",         desc = "add project" },
+				{ "<leader>pt", "<cmd>NvimTreeToggle<cr>",     desc = "file tree" },
 			}),
 
 			-- LSP
 			wk.add({
-				{ "<leader>l", group = "+lsp"},
-				{ "<leader>li", "<cmd>LspInfo<cr>", desc = "LSP info"},
-				{ "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "rename"},
-				{ "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "code action"},
-				{ "<leader>lo", "<cmd>Lspsaga outline<cr>", desc = "outline"},
-				{ "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "definition"},
-				{ "<leader>lD", "<cmd>lua vim.lsp.buf.declaration()<cr>", desc = "declaration"},
-				{ "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>", desc = "format"},
+				{ "<leader>l",  group = "+lsp" },
+				{ "<leader>li", "<cmd>LspInfo<cr>",                       desc = "LSP info" },
+				{ "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>",      desc = "rename" },
+				{ "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "code action" },
+				{ "<leader>lo", "<cmd>Lspsaga outline<cr>",               desc = "outline" },
+				{ "<leader>ld", "<cmd>lua vim.lsp.buf.definition()<cr>",  desc = "definition" },
+				{ "<leader>lD", "<cmd>lua vim.lsp.buf.declaration()<cr>", desc = "declaration" },
+				{ "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>",      desc = "format" },
 			}),
 			vim.keymap.set('n', '[e', '<cmd>Lspsaga show_line_diagnostics<cr>'),
 			vim.keymap.set('n', '[E', '<cmd>Lspsaga show_buf_diagnostics<cr>'),
@@ -48,30 +49,31 @@ return {
 
 			-- Git
 			wk.add({
-				{ "<leader>g", group = "+git"},
-				{ "<leader>gs", "<cmd>Git<cr>", desc = "status"},
-				{ "<leader>gc", "<cmd>Git commit<cr>", desc = "commit"},
-				{ "<leader>gp", "<cmd>Git push<cr>", desc = "push"},
-				{ "<leader>gl", "<cmd>Git pull<cr>", desc = "pull"},
-				{ "<leader>gb", "<cmd>Git blame<cr>", desc = "blame"},
-				{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "diff"},
+				{ "<leader>g",  group = "+git" },
+				{ "<leader>gs", "<cmd>Git<cr>",        desc = "status" },
+				{ "<leader>gc", "<cmd>Git commit<cr>", desc = "commit" },
+				{ "<leader>gp", "<cmd>Git push<cr>",   desc = "push" },
+				{ "<leader>gl", "<cmd>Git pull<cr>",   desc = "pull" },
+				{ "<leader>gb", "<cmd>Git blame<cr>",  desc = "blame" },
+				{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "diff" },
 			}),
 
 			-- Toggles
 			wk.add({
-				{ "<leader>t", group = "+toggle"},
-				{ "<leader>te", "<cmd>NvimTreeToggle<cr>", desc = "file explorer"},
-				{ "<leader>tT", "<cmd>Telescope<cr>", desc = "Telescope UI"},
-				{ "<leader>tt", group = "+terminal"},
-				{ "<leader>tth", "<cmd>ToggleTerm size=20 direction=horizontal<cr>", desc = "horizontal terminal"},
-				{ "<leader>ttv", "<cmd>ToggleTerm size=75 direction=vertical<cr>", desc = "vertical terminal"},
+				{ "<leader>t",  group = "+terminal" },
+				{ "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "horizontal terminal" },
+				{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "horizontal terminal" },
+				{ "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>",   desc = "vertical terminal" },
+				{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>",   desc = "vertical terminal" },
 			}),
+			vim.keymap.set('t', '<esc>', [[<C-\><C-n>]]),
+			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]]),
 
 			-- Completion
 			wk.add({
-				{ "<leader>c", group = "+completion" },
-				{ "<leader>cc", "<cmd>Copilot panel<cr>", desc="Copilot suggestions" },
-				{ "<leader>cl", "<cmd>Copilot panel<cr>", desc="Copilot suggestions" },
+				{ "<leader>c",  group = "+completion" },
+				{ "<leader>cc", "<cmd>Copilot panel<cr>", desc = "Copilot suggestions" },
+				{ "<leader>cl", "<cmd>Copilot panel<cr>", desc = "Copilot suggestions" },
 			}),
 		}
 	}
