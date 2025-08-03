750b972ce75c3919162b2bbb8b34d4f54004bd3e
Josh Burns <joshyburnss@gmail.com>
2025-08-04 00:12:20 +1000

    remove old csv package manifest

--- setup.sh
+++ setup.sh
@@ -6,37 +6,12 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Download package manifest
-curl https://raw.githubusercontent.com/bepisdev/dotfiles/refs/heads/main/pkg.csv > /tmp/pkg.csv
-
-# Create directories for later use
-mkdir $HOME/Code
-mkdir -p $HOME/.config/nvim
-
-# Process package manifest
-CSV_FILE="/tmp/pkg.csv"
-tail -n +2 "$CSV_FILE" | while IFS=',' read -r pkgname pkgtype clioptions
-do
-    echo "Installing Package: $pkgname"
-    echo "----"
-
-    case $pkgtype in
-        "formula")
-            brew install $pkgname $clioptions
-            ;;
-        "cask")
-            brew install --cask $pkgname $clioptions
-            ;;
-        "git")
-            git clone $pkgname $clioptions
-            ;;
-    esac
-done
+# Install Homebrew
+brew bundle ./Brewfile
 
 # Install dotfile packages
-pushd $HOME/Code/dotfiles
+mkdir -p $HOME/.config/{nvim,ghostty}
 stow zsh -t $HOME
 stow git -t $HOME
 stow nvim -t $HOME/.config/nvim
 stow ghostty -t $HOME/.config/ghostty
-popd
