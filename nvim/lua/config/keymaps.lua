1fe1b49a96aecc65c5b040f92d89bd20ff710c38
Josh Burns <joshyburnss@gmail.com>
2025-09-26 19:43:56 +1000

    devcontainer support and keymap group description

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+return {
+  "folke/which-key.nvim",
+  opts = {
+    defaults = {
+      ["<cmd>cD"] = { name = "+DevContainer" },
+    },
+  },
+}
--- nvim/lua/plugins/devcontainer.lua
+++ nvim/lua/plugins/devcontainer.lua
@@ -4,22 +4,24 @@ return {
     -- By default, if no extra config is added, following nvim_dotfiles are
     -- installed: "https://github.com/LazyVim/starter"
     -- This is an example for configuring other nvim_dotfiles inside the docker container
-    nvim_dotfiles_repo = "https://github.com/arnaupv/dotfiles.git",
-    nvim_dotfiles_install_command = "cd ~/nvim_dotfiles/ && ./install.sh",
-    -- In case you want to change the way the devenvironment is setup, you can also provide your own setup
-    setup_environment_repo = "https://github.com/arnaupv/setup-environment",
-    setup_environment_install_command = "./install.sh -p 'nvim stow zsh'",
+    nvim_dotfiles_repo = "https://github.com/bepisdev/dotfiles.git",
+    nvim_dotfiles_install_command = "cp -r ~/nvim_dotfiles/nvim ~/.config/nvim",
   },
   keys = {
     -- stylua: ignore
     {
-      "<leader>cdu",
-      ":DevcontainerUp<cr>",
+      "<leader>cD",
+      nil,
+      desc = "DevContainers",
+    },
+    {
+      "<leader>cDu",
+      "<cmd>DevcontainerUp<cr>",
       desc = "Up the DevContainer",
     },
     {
-      "<leader>cdc",
-      ":DevcontainerConnect<cr>",
+      "<leader>cDc",
+      "<cmd>DevcontainerConnect<cr>",
       desc = "Connect to DevContainer",
     },
   },
