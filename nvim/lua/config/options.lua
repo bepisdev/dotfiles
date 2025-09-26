288a5bcb6a60b0c4457b93993f6394dbc7798dc6
Josh Burns <joshyburnss@gmail.com>
2025-09-26 22:57:28 +1000

    cleanup stale code

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- Ruby LSP development
-vim.g.lazyvim_ruby_lsp = "ruby_lsp"
-vim.g.lazyvim_ruby_formatter = "rubocop"
